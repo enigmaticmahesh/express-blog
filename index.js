66c031caef69acf82df26e7a85144027e78997a1
enigmaticmahesh <challengermahesh@gmail.com>
2024-03-19 09:59:32 +0530

    Initial setup

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const nunjucks = require('nunjucks');
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+nunjucks.configure('views', {
+  autoescape: true,
+  express: app,
+});
+
+app.get('/', (req, res) => {
+  res.render('index.html');
+});
+
+app
+  .listen(PORT, () => {
+    console.log('Blog is running on PORT: ', PORT);
+  })
+  .on('error', (err) => {
+    console.log('Error while starting your blog: ', err);
+  });
